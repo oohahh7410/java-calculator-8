ff04047359c5568e8a4169c13f8364ebdfba7736
Kee <oohahh7410@gmail.com>
2025-10-18 03:08:30 +0900

    feat: 빈 문자열 또는 숫자 하나 입력 시 0 또는 해당 숫자 반환

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class StringCalculator {
+
+    public int calculate(String input) {
+
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return Integer.parseInt(input);
+    }
+}
